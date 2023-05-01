350334eb8392995d4db9b246b295b3ef54f1645d
medahad <medahad16@gmail.com>
2023-05-01 21:51:15 +0200

    Page Str Done

--- djCRM/urls.py
+++ djCRM/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import index
+from core.views import index, about
 
 urlpatterns = [
     path('', index, name="index"),
+    path('about/ ', about, name="about"),
     path('admin/', admin.site.urls),
 ]
