0f86dd568c9ca06fd7da7183011fbfa3d0182825
medahad <medahad16@gmail.com>
2023-05-21 23:08:25 +0200

    Added UpdateFunc

--- lead/urls.py
+++ lead/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.leads_list, name='leads_list'),
     path('<int:pk>/', views.leads_detail, name='leads_detail'),
+    path('<int:pk>/delete/', views.leads_delete, name='leads_delete'),
+    path('<int:pk>/edit/', views.leads_edit, name='leads_edit'),
     path('add-lead/', views.add_lead, name='add_lead'),
 ]
